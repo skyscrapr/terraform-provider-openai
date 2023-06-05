accc443e31edd04a3608d5526f889350814c7961
Richard Weerasinghe <rich.w@me.com>
2023-06-05 20:46:50 +1200

    add file resource to the provider

--- openai/provider.go
+++ openai/provider.go
@@ -79,7 +79,7 @@ func (p *OpenAIProvider) Configure(ctx context.Context, req provider.ConfigureRe
 
 func (p *OpenAIProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		// NewExampleResource,
+		NewFileResource,
 	}
 }
 
