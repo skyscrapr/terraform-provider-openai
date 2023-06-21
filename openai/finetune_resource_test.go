19921e6538f739778b22a8952c94b20102e7d058
Richard Weerasinghe <rich.w@me.com>
2023-06-21 22:19:34 +1200

    skip fine tune test

--- openai/finetune_resource_test.go
+++ openai/finetune_resource_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestAccFineTuneResource(t *testing.T) {
-	// t.Skip("Cost associated with test")
+	t.Skip("Cost associated with test")
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
