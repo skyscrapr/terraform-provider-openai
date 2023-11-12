ad4f94e1e2d0080519563ec83d903e9c127d6f9f
Richard Weerasinghe <richard@skyscrapr.io>
2023-11-13 09:15:49 +1300

    add examples to docs

--- examples/resources/openai_assistant/resource.tf
+++ examples/resources/openai_assistant/resource.tf
@@ -7,13 +7,13 @@ terraform {
 }
 
 resource "openai_assistant" "test" {
-	name = "tf-test-sample"
-	description = "tf-test-sample"
-	model = "gpt-4"
-	instructions = "You are a personal math tutor. When asked a question, write and run Python code to answer the question."
-	tools = [
-		{
-            type = "code_interpreter"
-        }
-	]
+  name         = "tf-test-sample"
+  description  = "tf-test-sample"
+  model        = "gpt-4"
+  instructions = "You are a personal math tutor. When asked a question, write and run Python code to answer the question."
+  tools = [
+    {
+      type = "code_interpreter"
+    }
+  ]
 }
