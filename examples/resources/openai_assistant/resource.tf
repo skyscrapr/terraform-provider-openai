7554b7648835b3ed569b6bcfd3c23cbcc7ef4ec3
Richard Weerasinghe <richard@skyscrapr.io>
2023-11-13 09:14:53 +1300

    restructure examples to generate docs

---
+++ examples/resources/openai_assistant/resource.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    openai = {
+      source = "skyscrapr/openai"
+    }
+  }
+}
+
+resource "openai_assistant" "test" {
+	name = "tf-test-sample"
+	description = "tf-test-sample"
+	model = "gpt-4"
+	instructions = "You are a personal math tutor. When asked a question, write and run Python code to answer the question."
+	tools = [
+		{
+            type = "code_interpreter"
+        }
+	]
+}
